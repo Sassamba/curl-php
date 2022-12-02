0f35bcf421512295d4f9aa4b01e692db0785a34b
Sassamba <39568237+Sassamba@users.noreply.github.com>
2022-12-02 10:44:51 +0100

    Disable proxy

--- main.php
+++ main.php
@@ -3,12 +3,12 @@ $curl = curl_init($file);
 
 curl_setopt($curl, CURLOPT_URL, $file);
 
-    curl_setopt($curl, CURLOPT_HEADER, false);
+curl_setopt($curl, CURLOPT_HEADER, false);
 
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+//Set a proxy if there is any
+//curl_setopt($curl, CURLOPT_PROXY, "proxy-url");
 
-    curl_setopt($curl, CURLOPT_PROXY, "http://proxy.jupiter.co.ao:3128");
+$dados = curl_exec($curl);
 
-    $dados = curl_exec($curl);
-
-    curl_close($curl);
+curl_close($curl);
